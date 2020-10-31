8f4b14362e331d1263711d5270dc2c93d064c185
Eri Sitohang <erisitohang@users.noreply.github.com>
2020-10-31 19:16:37 +0800

    Added migration functionality using phinx

---
+++ phinx.php
@@ -0,0 +1,44 @@
+<?php
+use \Symfony\Component\Dotenv\Dotenv;
+$dotenv = new Dotenv(true);
+$dotenv->load(__DIR__ . '/.env');
+
+return
+[
+    'paths' => [
+        'migrations' => '%%PHINX_CONFIG_DIR%%/db/migrations',
+        'seeds' => '%%PHINX_CONFIG_DIR%%/db/seeds'
+    ],
+    'environments' => [
+        'default_migration_table' => 'phinxlog',
+        'default_environment' => 'development',
+        'production' => [
+            'adapter' => getenv('DB_ADAPTER'),
+            'host' => getenv('DB_HOST'),
+            'name' => getenv('DB_NAME'),
+            'user' => getenv('DB_USERNAME'),
+            'pass' => getenv('DB_PASSWORD'),
+            'port' => getenv('DB_PORT'),
+            'charset' => 'utf8',
+        ],
+        'development' => [
+            'adapter' => getenv('DB_ADAPTER'),
+            'host' => getenv('DB_HOST'),
+            'name' => getenv('DB_NAME'),
+            'user' => getenv('DB_USERNAME'),
+            'pass' => getenv('DB_PASSWORD'),
+            'port' => getenv('DB_PORT'),
+            'charset' => 'utf8',
+        ],
+        'testing' => [
+            'adapter' => getenv('DB_ADAPTER'),
+            'host' => getenv('DB_HOST'),
+            'name' => getenv('DB_NAME'),
+            'user' => getenv('DB_USERNAME'),
+            'pass' => getenv('DB_PASSWORD'),
+            'port' => getenv('DB_PORT'),
+            'charset' => 'utf8',
+        ]
+    ],
+    'version_order' => 'creation'
+];
